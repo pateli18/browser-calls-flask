0b4b4d76584db443d34f3c39ed73d6004cd4cafd
Victor Shyba <victor.shyba@gmail.com>
2016-03-14 18:17:46 -0300

    Fix build for py 3.4

--- browser_calls_flask/__init__.py
+++ browser_calls_flask/__init__.py
@@ -1,4 +1,3 @@
-import os
 from browser_calls_flask.config import config_env_files
 from flask import Flask
 
@@ -6,12 +5,14 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def create_app(config_name='development', p_db=db):
     new_app = Flask(__name__)
     config_app(config_name, new_app)
 
     p_db.init_app(new_app)
     return new_app
+
 
 def config_app(config_name, new_app):
     new_app.config.from_object(config_env_files[config_name])
