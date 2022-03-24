231b96606fe36518a1e8243ef6a9d48e03289d70
Ihsaan Patel <ihsaan.patel@gmail.com>
2022-03-24 16:08:10 -0700

    update port

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from browser_calls_flask import app, db
 from browser_calls_flask.models import SupportTicket
@@ -7,6 +7,7 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+manager.add_command("runserver", Server(host="0.0.0.0", port=5002))
 
 
 @manager.command
@@ -25,7 +26,7 @@ def test():
 @manager.command
 def dbseed():
     ticket1 = SupportTicket(
-        name="John", phone_number="+15551239483", description="Got an issue while..."
+        name="Ihsaan", phone_number="+13612326374", description="Got an issue while..."
     )
     db.session.add(ticket1)
     db.session.commit()
