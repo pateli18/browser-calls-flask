26e5e4adb03daa3a0241623d54e37771df4f9fc7
Ihsaan Patel <ihsaan.patel@gmail.com>
2022-03-24 18:58:21 -0700

    remove unnecessary flask stuff

--- browser_calls_flask/__init__.py
+++ browser_calls_flask/__init__.py
@@ -1,10 +1,7 @@
 from browser_calls_flask.config import config_classes
 from flask import Flask
 
-from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-
-db = SQLAlchemy()
 
 
 # create and configure the app
@@ -15,7 +12,5 @@ Bootstrap(app)
 # load the instance config, if it exists, when not testing
 env = app.config.get('ENV', 'production')
 app.config.from_object(config_classes[env])
-
-db.init_app(app)
 
 from . import views  # noqa E402
